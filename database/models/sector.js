4b6d44c82a92d30b4c620c427a8f1f6cdd44f992
Sneha <snehalall.sneha@gmail.com>
2023-02-03 12:23:31 +0530

    feat : create sector model

---
+++ database/models/sector.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+    Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+    class Sector extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+        static associate(models) {
+            Sector.hasOne(models.Company, {
+                as: 'company',
+                foreignKey: 'sector_id',
+            });
+        }
+    }
+    Sector.init({
+        name: DataTypes.STRING
+    }, {
+        sequelize,
+        modelName: 'Sector',
+    });
+    return Sector;
+};
