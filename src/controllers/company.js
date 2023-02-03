5bb97ad2388f0158028f8c80a1c7d0e9f549da03
Sneha <snehalall.sneha@gmail.com>
2023-02-03 15:42:52 +0530

    fix: remove console.log

--- src/controllers/company.js
+++ src/controllers/company.js
@@ -3,7 +3,6 @@ const getData = async (req, res) => {
     try {
         const body = req.body;
         const data = await companyService.getData(body);
-        console.log(data);
         res.status(201).json(data.map((company) => { return { id: company.id, name: company.name, score: company.score }; }));
     } catch (error) {
         res.status(500).json({
