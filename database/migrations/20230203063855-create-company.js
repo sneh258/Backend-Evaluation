5999561bfeff4727e5ccb21e611ad7fe12fa32b6
Sneha <snehalall.sneha@gmail.com>
2023-02-03 12:20:59 +0530

    feat : add company migration file

---
+++ database/migrations/20230203063855-create-company.js
@@ -0,0 +1,44 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable('Companies', {
+            id: {
+                allowNull: false,
+                primaryKey: true,
+                type: Sequelize.UUID
+            },
+            name: {
+                type: Sequelize.STRING,
+            },
+            ceo: {
+                type: Sequelize.STRING
+            },
+            description: {
+                type: Sequelize.STRING
+            },
+            score: {
+                type: Sequelize.INTEGER,
+                defaultValue:0
+            },
+            sector_id: {
+                type: Sequelize.INTEGER
+            },
+            address: {
+                type: Sequelize.STRING,
+                allowNull: true,
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        });
+    },
+    async down(queryInterface, Sequelize) {
+        await queryInterface.dropTable('Companies');
+    }
+};
