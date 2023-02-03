5ee561b76bbbf3c0c95ed7aca53fb573783a6c3e
Sneha <snehalall.sneha@gmail.com>
2023-02-03 14:54:15 +0530

    fix : remove a file

--- src/server.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const { companyRoute } = require('./routes');
-const app = express();
-
-app.use(express.json());
-app.use('/api', companyRoute.router);   
-module.exports = app;
