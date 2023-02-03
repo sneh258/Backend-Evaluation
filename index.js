5af376c54c2a940a64797e67a694c3b6921e11cf
Sneha <snehalall.sneha@gmail.com>
2023-02-03 11:48:40 +0530

    chore : started a server

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+app.use(express.json());
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`server started listening on port ${port}`);
+});
+
+
+
+
