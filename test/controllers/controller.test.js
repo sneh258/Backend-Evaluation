0df564f513f4077dbf11ee40477a8eefc341aed5
Sneha <snehalall.sneha@gmail.com>
2023-02-03 14:29:40 +0530

    test : add test cases for controller

---
+++ test/controllers/controller.test.js
@@ -0,0 +1,48 @@
+const getControllers = require('./../../src/controllers/company');
+const getServices = require('./../../src/services/company');
+
+describe('Company Controllers', () => {
+    it('should return an array of objects', async () => {
+        jest.spyOn(getServices, 'save').mockResolvedValue([
+            {
+                id: 1
+            }
+        ]
+        );
+
+        const mockReq = {};
+
+        const mockRes = {
+
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn()
+
+        };
+        await getControllers.save(mockReq, mockRes);
+        expect(mockRes.status).toBeCalledWith(200);
+        expect(mockRes.json).toBeCalledWith(
+            [
+                {
+                    id: 1
+                }
+            ]
+        );
+    });
+    it('should return error', async () => {
+        jest.spyOn(getServices, 'save').mockRejectedValue(new Error('Internal Server error!!'));
+
+        const mockReq = {};
+
+        const mockRes = {
+
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn()
+
+        };
+        await getControllers.save(mockReq, mockRes);
+        expect(mockRes.status).toBeCalledWith(500);
+        expect(mockRes.json).toBeCalledWith({
+            error: 'Internal Server error!!'
+        });
+    });
+});
