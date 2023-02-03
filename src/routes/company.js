00d26a130e0e40d7de83ce9abef96b121c0615bb
Sneha <snehalall.sneha@gmail.com>
2023-02-03 15:13:52 +0530

    fix : remove extra paranthesis

--- src/routes/company.js
+++ src/routes/company.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const { companyController } = require('../controllers');
+const  companyController = require('../controllers/company');
 const middlewares = require('../middlewares');
 const router = express.Router();
 
 router.route('/save')
-    .post(middlewares.bodyValidation, companyController.save);
+    .post(middlewares.bodyValidation, companyController.getData);
 
-module.exports = {router};
+module.exports = router;
