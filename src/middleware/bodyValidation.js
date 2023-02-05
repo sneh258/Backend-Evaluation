3ec7dcf25acf4307ec0d4c31641fdc01b5a933a3
Sneha <snehalall.sneha@gmail.com>
2023-02-06 00:39:05 +0530

    feat : add body, id and sector validation using JOI

--- src/middleware/bodyValidation.js
+++ src/middleware/bodyValidation.js
@@ -12,4 +12,30 @@ const bodyValidation = (req, res, next) => {
     next();
 };
 
-module.exports = bodyValidation;
+const sectorValidation = (req, res, next) => {
+    const schema = Joi.object({
+        sector: Joi.string().required()
+    });
+    const { error } = schema.validate(req.query);
+    if(error) {
+        res.status(400).json({ message: error.message });
+        return;
+    }
+    next();
+};
+
+const idValidation = (req, res, next) => {
+    const schema = Joi.object({
+        id: Joi.string().required()
+    });
+    const { error } = schema.validate(req.query);
+    if(error) {
+        res.status(400).json({ message: error.message });
+        return;
+    }
+    next();
+};
+
+
+
+module.exports = {bodyValidation,sectorValidation,idValidation};
