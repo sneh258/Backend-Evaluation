1165c79c8626a81d3a58760f49f46dab6d35d28e
Sneha <snehalall.sneha@gmail.com>
2023-02-06 00:38:13 +0530

    feat : add controllers to update company details

--- src/controllers/company.js
+++ src/controllers/company.js
@@ -1,4 +1,5 @@
 const companyService  = require('../services/company');
+
 const getData = async (req, res) => {
     try {
         const body = req.body;
@@ -11,4 +12,35 @@ const getData = async (req, res) => {
     }
 };
 
-module.exports = getData;
+const getScore=async(req,res)=>{
+
+    try {
+        const query = req.query.sector;
+        const data = await companyService.getScore(query);
+        const companies = data.data.company;
+        //sorting is needed to be done
+        res.status(200).json(companies.map((company, index) => { return { id: company.id, name: company.name, ceo: company.ceo, score: company.score, rank: index + 1 }; }));
+    } catch (error) {
+        res.status(500).json({
+            error: error.message
+        });
+    }
+
+
+};
+
+const updateCompany = async (req, res) => {
+    try {
+        const id = req.query.id;
+        const bodyCeo = req.body.ceo;
+        const bodyAddress=req.body.bodyAddress;
+        const data = await companyService.updateCompanyDetails(id, bodyCeo,bodyAddress);
+        res.status(200).json(data);
+    } catch (error) {
+        res.status(500).json({
+            error: error.message
+        });
+    }
+};
+
+module.exports = {getData,getScore,updateCompany};
