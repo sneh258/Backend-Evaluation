40edeb906be4abbe666dcae1085077a452ce1171
Sneha <snehalall.sneha@gmail.com>
2023-02-06 00:41:13 +0530

    feat : add services to update company details

--- src/services/company.js
+++ src/services/company.js
@@ -1,54 +1,71 @@
 const axios = require('axios');
 const { Company, Sector } = require('../../database/models');
-const utils = require('../utils/readData');
-const handleCompanyURL='http://54.167.46.10/company/95b5a067-808a-44a9-a490-b4ef8a045f61';
-const handleSectorURL='http://54.167.46.10/sector?name=Software';
+const readData = require('../utils/readData');
+const splitString = require('../utils/splitString');
+const handleCompanyURL='http://54.167.46.10/company/';
+const handleSectorURL='http://54.167.46.10/sector?name=';
 
 const getData = async () => {
-    const csv = await axios.get('https://store-0001.s3.amazonaws.com/input.csv');
-    const lines = utils.readData(csv);
+    const csv = await axios.get('https://store-0001.s3.amazonaws.com/input.csv');//read
+    const lines = readData(csv);//split
 
-    lines.forEach(async (line, index) => {
+    lines.forEach(async (line, index) => { //traverse for company creation (company table)
         if (index === 0) {
             return;
         }
-        const data = utils.splitString(line);
-        const companyData = await axios.get(`${handleCompanyURL}${data[0]}`);
+        const data = splitString(line); // to split id and sector
+        const companyData = await axios.get(`${handleCompanyURL}${data[0]}`); //get id and sector
         
         
         const company = {
-            id: companyData.id,
-            name: companyData.name,
-            ceo: companyData.ceo,
-            description: companyData.description,
+            id: companyData.data.id,
+            name: companyData.data.name,
+            ceo: companyData.data.ceo,
+            //description: companyData.data.description,
         };
         await Company.create(company);
     });
 
 
 
-    lines.forEach(async (line, index) => {
+    lines.forEach(async (line, index) => {// (sector table)
         if (index === 0)
             return;
-        const data = utils.splitString(line);
+        const data = splitString(line);//split  by commas
         const sectorData = await axios.get(`${handleSectorURL}${data[1]}`);
         const sector = {
             name: data[1],
         };
-        const newSector = await Sector.create(sector);
+        const newSector = await Sector.create(sector);// filling sector table
 
         sectorData.data.forEach(async (secData) => {
             let index = 0;
             const score = ((Number(secData.performanceIndex[index++].value) * 10) + (Number(secData.performanceIndex[index++].value) / 10000) + (Number(secData.performanceIndex[index++].value) * 10) + Number(secData.performanceIndex[index++].value)) / 4;
 
-            await Company.update({ sector_id: newSector.data.id, score: score }, {
+            await Company.update({ sector_id: newSector.id, score: score }, {
                 where: {
                     id: secData.companyId
                 }
             });
         });
+        
     });
     return Company.findAll();
 };
 
-module.exports = getData;
+
+const getScore = async (query) => {
+    const found=Sector.findOne({ where: { name: query }, include: [ 'company' ] });
+    return found;
+};
+
+const updateCompanyDetails = async (id, bodyCeo,bodyAddress) => {
+    const company = await Company.findOne({ where: { id: id } });
+    if(!company)
+        return {};
+        
+    Company.update({ ceo: bodyCeo, address: bodyAddress }, { where: { id: id } });
+    return Company.findAll({where:{id:id}});
+};
+
+module.exports = {getData,getScore,updateCompanyDetails};
