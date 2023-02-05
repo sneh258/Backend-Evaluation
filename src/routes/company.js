ce02c4ee20a94cdd051d5cef9ac9ef670052d34b
Sneha <snehalall.sneha@gmail.com>
2023-02-06 00:39:40 +0530

    feat: add routes -get and patch

--- src/routes/company.js
+++ src/routes/company.js
@@ -1,9 +1,15 @@
 const express = require('express');
-const  companyController = require('../controllers/company');
+const companyController = require('../controllers/company');
 const middleware = require('../middleware/bodyValidation');
 const router = express.Router();
 
 router.route('/save')
-    .post(middleware, companyController.getData);
+    .post(middleware.bodyValidation, companyController.getData);
+
+router.route('/companies')
+    .get(middleware.sectorValidation,companyController.getScore);
+    
+router.route('/update')
+    .patch(middleware.idValidation,companyController.updateCompany);    
 
 module.exports = router;
